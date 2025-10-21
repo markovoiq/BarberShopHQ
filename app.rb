7c8426e740b1ef51db2815d16627278cc5550568
rubyst <markovoiq@outlook.com>
2025-10-21 13:32:02 +0300

    Add visit view

--- app.rb
+++ app.rb
@@ -12,7 +12,26 @@ end
 class Barber <ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
 end
